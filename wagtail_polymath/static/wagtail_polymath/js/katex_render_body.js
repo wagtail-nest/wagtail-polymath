90ed1a880c08f7bd04d21852e6314870c2b2ec2f
Chih Wang <ongchi@users.noreply.github.com>
2024-05-12 10:05:30 +0800

    Add templates tags wagtail_polymath_css and wagtail_polymath_js

---
+++ wagtail_polymath/static/wagtail_polymath/js/katex_render_body.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  renderMathInElement(
+    document.body,
+    {
+      delimiters: [
+        {left: '$$', right: '$$', display: true},
+        {left: '$', right: '$', display: false},
+        {left: '\\(', right: '\\)', display: false},
+        {left: '\\[', right: '\\]', display: true}
+      ],
+      throwOnError : false
+    }
+  );
+});
--- wagtail_polymath/templatetags/wagtail_polymath.py
+++ wagtail_polymath/templatetags/wagtail_polymath.py
@@ -1,11 +1,30 @@
 from django import template
+from django.templatetags.static import static
+from django.utils.html import format_html, format_html_join
 
-from wagtail_polymath.settings import WAGTAILPOLYMATH_SETTINGS
+from ..settings import WAGTAILPOLYMATH_ENGINE
+from ..wagtail_hooks import polymath_config_js, polymath_css, polymath_js
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def mathjax():
-    return WAGTAILPOLYMATH_SETTINGS["js"]
+def wagtail_polymath_css():
+    return polymath_css()
+
+
+@register.simple_tag
+def wagtail_polymath_js():
+    js = [(polymath_js(),), (polymath_config_js(),)]
+    if WAGTAILPOLYMATH_ENGINE == "katex":
+        js.append(
+            (
+                format_html(
+                    '<script src="{}"></script>',
+                    static("wagtail_polymath/js/katex_render_body.js"),
+                ),
+            )
+        )
+
+    return format_html_join("\n", "{0}", js)
